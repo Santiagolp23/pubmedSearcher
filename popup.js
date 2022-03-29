4a1481dd25293e388043d4a2225eb6b688c031a9
Santiagolp23 <santiagolopezpardo@gmail.com>
2022-03-28 21:50:48 -0500

    first commit

---
+++ popup.js
@@ -0,0 +1,26 @@
+var search = document.getElementById("submitbutton");
+search.addEventListener("click", doSearch);
+
+document.getElementById("inputbox").focus();
+
+document.addEventListener("keyup", doSearchKey);
+
+
+function doSearch() {
+    var nameOfPerson = document.getElementById("inputbox").value;
+    nameOfPerson = nameOfPerson.replace(/\s/g, '+');
+    var finalLink = "https://pubmed.ncbi.nlm.nih.gov/?term=" + nameOfPerson + "&format=abstract&sort=date&size=100";
+    window.open(finalLink);
+
+}
+
+function doSearchKey(event) {
+
+    if (event.keyCode == 13) {
+        var nameOfPerson = document.getElementById("inputbox").value;
+        nameOfPerson = nameOfPerson.replace(/\s/g, '+');
+        var finalLink = "https://pubmed.ncbi.nlm.nih.gov/?term=" + nameOfPerson + "&format=abstract&sort=date&size=100";
+        window.open(finalLink);
+    }
+    else {}
+}
